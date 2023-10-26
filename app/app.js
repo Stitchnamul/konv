88971cdcdd052b148104f408fe2db727dd9ec376
Stitchnamul <hyezim13@naver.com>
2023-10-26 15:25:40 +0900

    express 연습

--- app/app.js
+++ app/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', router)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+router.get('/air',(req,res) => {
+  console.log('/api/air 왔음')
+  res.send('air')
 })
+
+app.listen(port);
+
+
